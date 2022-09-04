4f8dc873ef8c784955d59f2f9d43eff01cf13449
SunaChoi <102567671+suna99@users.noreply.github.com>
2022-09-04 12:47:52 +0900

    2022.09.04
    react

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client"; //싱글
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
